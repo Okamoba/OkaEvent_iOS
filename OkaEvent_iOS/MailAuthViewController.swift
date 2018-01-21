7032ccec598aeb97660accc9c1f0ffb3952b7534
石川諒 <ishikawa.pro@gmail.com>
2018-01-21 15:11:38 +0900

    add MailAuthViewController.swift

---
+++ OkaEvent_iOS/MailAuthViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MailAuthViewController.swift
+//  OkaEvent_iOS
+//
+//  Created by 石川諒 on 2018/01/21.
+//  Copyright © 2018年 talkie. All rights reserved.
+//
+
+import UIKit
+
+class MailAuthViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
