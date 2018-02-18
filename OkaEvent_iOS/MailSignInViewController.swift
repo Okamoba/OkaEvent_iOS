3b37f256c5083b75c33bf5f4bca7e6a236560b25
石川諒 <ishikawa.pro@gmail.com>
2018-02-18 14:41:18 +0900

    サインアップの機能を追加

--- OkaEvent_iOS/MailSignInViewController.swift
+++ OkaEvent_iOS/MailSignInViewController.swift
@@ -8,9 +8,29 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
 
 class MailSignInViewController: UIViewController {
+    @IBOutlet weak var mailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    @IBAction func SignUpTapped(_ sender: Any) {
+        guard let mailAddress = mailTextField.text else {
+            return
+        }
+        guard let password = passwordTextField.text else {
+            return
+        }
+        Auth.auth().createUser(withEmail: mailAddress, password: password) { (user, error) in
+            if let error = error {
+                print(error)
+            }
+            let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+            let mainViewController = storyboard.instantiateViewController(withIdentifier: "MainViewController")
+            self.present(mainViewController, animated: true, completion: nil)
+        }
+    }
+
 }
