954546256e398c6615c29f4d2ee65dfdc24c8c44
jangom2ok <jangom2ok@gmail.com>
2018-03-11 17:02:05 +0900

    MainViewにイベント一覧用のリストを追加

--- OkaEvent_iOS/MainViewController.swift
+++ OkaEvent_iOS/MainViewController.swift
@@ -9,7 +9,11 @@
 import UIKit
 import FirebaseAuth
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource
+ {
+    @IBOutlet weak var TreeView: UITableView!
+    //配列fruitsを設定
+    let fruits = ["apple", "orange", "melon", "banana", "pineapple"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -36,6 +40,18 @@ class ViewController: UIViewController {
             mailAuthViewControllerTransition()
         }
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fruits.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // セルを取得する
+        let cell: UITableViewCell = tableView.dequeueReusableCell(withIdentifier: "SampleCell", for: indexPath)
+        
+        // セルに表示する値を設定する
+        cell.textLabel!.text = fruits[indexPath.row]
+        
+        return cell
+    }
 }
 
