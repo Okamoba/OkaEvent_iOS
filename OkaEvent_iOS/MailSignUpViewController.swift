008a4eb3ae2c7c25c40a51ab7021adbfcd02bb0f
ishikawa-pro <ishikawa.pro@gmail.comm>
2018-04-28 16:20:01 +0900

    add displayName input form to SignUpViewController

--- OkaEvent_iOS/MailSignUpViewController.swift
+++ OkaEvent_iOS/MailSignUpViewController.swift
@@ -11,12 +11,38 @@ import UIKit
 import FirebaseAuth
 
 class MailSignUpViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+    
+    func setDisplayName() {
+        guard let name = nameTextField.text else {
+            return
+        }
+        let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+        //displayNameのセット
+        changeRequest?.displayName = name
+        changeRequest?.commitChanges { (error) in
+            if let e = error {
+                print(e)
+                return
+            }
+        }
+        
+        //MainViewControllerへ遷移
+        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let mainViewController = storyboard.instantiateViewController(withIdentifier: "MainViewController")
+        self.present(mainViewController, animated: true, completion: nil)
+    }
+    
     @IBAction func SignUpTapped(_ sender: Any) {
+        guard nameTextField.text != nil else {
+            return
+        }
         guard let mailAddress = mailTextField.text else {
             return
         }
@@ -27,9 +53,7 @@ class MailSignUpViewController: UIViewController {
             if let error = error {
                 print(error)
             }
-            let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
-            let mainViewController = storyboard.instantiateViewController(withIdentifier: "MainViewController")
-            self.present(mainViewController, animated: true, completion: nil)
+            self.setDisplayName()
         }
     }
 
