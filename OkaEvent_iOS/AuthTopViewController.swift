8bd3732b604542de15b5040305e4de3645bc7b9c
ishikawa-pro <ishikawa.pro@gmail.com>
2018-07-22 20:23:57 +0900

    Add code that transition to EmailAuthView

--- OkaEvent_iOS/AuthTopViewController.swift
+++ OkaEvent_iOS/AuthTopViewController.swift
@@ -7,7 +7,31 @@
 //
 
 import UIKit
+import FirebaseAuthUI
 
-class AuthTopViewController: UIViewController {
+class AuthTopViewController: UIViewController, FUIAuthDelegate {
     
+    var authUI :FUIAuth?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        authUI = FUIAuth.defaultAuthUI()
+        authUI?.delegate = self
+    }
+    
+    @IBAction func EmailAuthButtonTapped(_ sender: Any) {
+        let authVC = authUI?.authViewController()
+        present(authVC!, animated: true, completion: nil)
+    }
+    
+    func authUI(_ authUI: FUIAuth, didSignInWith authDataResult: AuthDataResult?, error: Error?) {
+        if let error = error {
+            print(error)
+        }
+        if authDataResult?.user != nil {
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let MainVC = storyboard.instantiateViewController(withIdentifier: "MainViewController")
+            present(MainVC, animated: true, completion: nil)
+        }
+    }
 }
