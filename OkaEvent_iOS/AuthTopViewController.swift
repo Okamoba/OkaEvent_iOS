6b891012a51b4179187b057e1e215f0f26dd5555
ishikawa-pro <ishikawa.pro@gmail.com>
2018-07-22 16:38:46 +0900

    add AuthTopViewController

---
+++ OkaEvent_iOS/AuthTopViewController.swift
@@ -0,0 +1,13 @@
+//
+//  AuthTopViewController.swift
+//  OkaEvent_iOS
+//
+//  Created by Ishikawa Akira on 2018/07/22.
+//  Copyright © 2018年 talkie. All rights reserved.
+//
+
+import UIKit
+
+class AuthTopViewController: UIViewController {
+    
+}
